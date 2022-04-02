078652a1fe008c2c3de67828cc391f13ff8b7c9c
Noah Bruno <noah.bruno122@gmail.com>
2022-04-02 13:33:38 -0500

    color

---
+++ main/WebApp/pickcolor.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: noahbruno
+  Date: 4/1/22
+  Time: 11:31 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Welcome to PickColor!</h1>
+
+<form method="post">
+    <label for="color">Choose a color: </label>
+    <input id="color" name="color">
+    <button type="submit">This Color!</button>
+</form>
+
+</body>
+</html>
---
+++ main/java/ViewColor.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="ViewColor", urlPatterns = "/ViewColor")
+public class ViewColor extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String color = request.getParameter("color");
+        request.setAttribute("color", color);
+        request.getRequestDispatcher("viewcolor.jsp").forward(request,response);
+
+
+    }
+}
