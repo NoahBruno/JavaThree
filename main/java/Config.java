3cef8a606e6a2e857edd57553d8030b64950cd2e
Noah Bruno <noah.bruno122@gmail.com>
2022-04-08 11:45:44 -0500

    new

--- main/java/Config.java
+++ main/java/Config.java
@@ -1,6 +1,6 @@
 public class Config {
     public String getURL() {
-        return "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false";
+        return "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
     }
     public String getUser() {
         return "root";
---
+++ main/java/MySQLUsersDao.java
@@ -0,0 +1,8 @@
+import models.User;
+
+import java.util.List;
+
+public interface MySQLUsersDao {
+    User findByUsername(String username);
+    Long insert(User user);
+}
